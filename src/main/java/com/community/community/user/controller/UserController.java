037cfcf82db9ab06c383370221d123f7c8bd3c90
kwonohtae <quite84@gmail.com>
2026-01-16 16:46:08 +0900

    폴더/파일 명 변경

--- src/main/java/com/community/community/user/controller/UserController.java
+++ src/main/java/com/community/community/user/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.Board.BoardTest.user.controller;
+package com.community.community.user.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
