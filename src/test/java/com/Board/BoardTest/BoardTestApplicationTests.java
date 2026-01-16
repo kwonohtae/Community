7dfc96b2857082f147a583ba03c6b76f96993c30
kwonohtae <quite84@gmail.com>
2026-01-16 13:43:39 +0900

    첫 commit

---
+++ src/test/java/com/Board/BoardTest/BoardTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Board.BoardTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BoardTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
